697d97acc2a309904fee5f63f947d1abfe14ce7a
Hari Prasad <prasad19.hari@gmail.com>
2017-02-01 19:32:58 +0530

    COntent update

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,45 @@
 (function() {
     'use strict';
-var app =angular.module('myApp',['ngMaterial','ngMessages','ngAnimate','ngAria','ui.router']).config(stateConfig)
-.config(defaultRouteConfig);
-app.controller('formController',function($scope,$mdDialog){
-	$scope.forget=function(ev){
-		$mdDialog.show({
-			controller:DialogController,
-			templateUrl:'/templates/forget.html',
-			parent:angular.element(document.body),
-			targetEvent:ev,
-			clickOutsideToClose:true
-		});
-	}
-	function DialogController($scope,$mdDialog){
-	$scope.cancel = function() {
-      $mdDialog.cancel();
-    };
+    var app = angular.module('myApp', ['ngMaterial', 'ngMessages', 'ngAnimate', 'ngAria', 'ui.router'])
+        .config(stateConfig)
+        .config(defaultRouteConfig);
+//     .config(['$locationProvider', function($locationProvider) {
+// $locationProvider.html5Mode(true);
+// $locationProvider.hashPrefix('!');
+// }]);
+    app.controller('formController', function($scope, $mdDialog) {
+        $scope.forget = function(ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                templateUrl: '/templates/forget.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+        }
 
-}
+        function DialogController($scope, $mdDialog) {
+            $scope.cancel = function() {
+                $mdDialog.cancel();
+            };
+
+        }
 
 
-	
 
-});
 
-function defaultRouteConfig($urlRouterProvider) {
+    });
+
+    function defaultRouteConfig($urlRouterProvider) {
 
         $urlRouterProvider.otherwise('/');
 
-    } 
+    }
+
+    
+
     function stateConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-        
+
         $urlRouterProvider.when('/', '/home');
 
         $stateProvider
@@ -38,6 +47,10 @@ function defaultRouteConfig($urlRouterProvider) {
                 url: "/home",
                 templateUrl: 'templates/home/home.html'
             });
+        $stateProvider.state('second', {
+            url: "/second",
+            templateUrl: 'templates/second/second.html'
+        })
 
 
     }
